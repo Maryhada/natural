5fac733f57cebe2b3069bc2ed26eabc9ec10953e
Treasure <treasure.olanipekun@gmail.com>
2026-01-31 11:22:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#input the value of terms
+n = int(input("Enter the value of terms: "))
+
+sum = 0 #intitialise
+i = 1 #initialise
+while i<=n: #loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+
+    print("/nSum =", sum)
